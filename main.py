e158936fb530f61044f9cf0d819642b8d09aed61
Xavier901 <39602489+Xavier901@users.noreply.github.com>
2025-04-28 02:05:31 +0530

    user setup

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
+  
+user = {1, "Bapi", "bapi@email.com"}
+  
+   
 
 @app.get("/")
 def read_root():
-    return {'Data': "Hello World"}
+    return {'user':{'id': 1,
+                    'name':'bapi',
+                    'email':'bapi@email.com'} }
