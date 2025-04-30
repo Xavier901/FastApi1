5e2c4bbb303ef25d322096195e690379ef5e136f
Xavier901 <39602489+Xavier901@users.noreply.github.com>
2025-05-01 03:57:50 +0530

    test sucessfull at http://127.0.01:8000 port

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
+
+
+origins = [
+    #"http://localhost:8080",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
   
 user = {1, "Bapi", "bapi@email.com"}
   
    
 
-@app.get("/")
+@app.get("/users")
 def read_root():
-    return {'user':{'id': 1,
+    return  [{'id': 1,
                     'name':'bapi',
-                    'email':'bapi@email.com'} }
+                    'email':'bapi@email.com' }
+             ]
+    # return  {   'name':'bapi'  }
+             
